9f535d460c84757b2884aed073f755b576165b7d
Kevin Tan <kevin@ktizzel.com>
2016-04-08 23:50:25 -0400

    impossible quiz, rap vocab and most used words

--- puzzles/impossible/puzzle1i.py
+++ puzzles/impossible/puzzle1i.py
@@ -22,4 +22,29 @@
 # ------------------------------------ 
 # INSTRUCTIONS:
 
+f = open('rap.txt', 'r+')
+
+def unique_words():
+    words = {}
+    for line in f:
+        line = line.replace(',', '')
+        line_stripped = [x.strip() for x in line.split(' ')]
+        for word in line_stripped:
+            if(word in words):
+                words[word] += 1
+            else:
+                words[word] = 1
+    return words
+
+def num_unique_words():
+    words = unique
+    return len(words)
+
+def top_words():
+    words = unique_words()
+    import operator
+    sorted_words = sorted(words.items(), key=operator.itemgetter(1))
+    sorted_words_desc = sorted_words[::-1]
+    top_ten = sorted_words_desc[:10]
+    return top_ten
 
