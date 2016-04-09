bd0ff173253c75557be7577516442c0ce320af58
Kevin Tan <kevin@ktizzel.com>
2016-04-09 00:07:31 -0400

    no answers

--- puzzles/impossible/puzzle1i.py
+++ puzzles/impossible/puzzle1i.py
@@ -1,11 +1,11 @@
-# ------------------------------------ 
+# ------------------------------------
 # CODE BOOLA 2015 PYTHON WORKSHOP
 # Mike Wu, Jonathan Chang, Kevin Tan
 # Puzzle Challenges Number 1
-# ------------------------------------ 
+# ------------------------------------
 
 # Crafted to really give you a hard
-# time. Please please please do not 
+# time. Please please please do not
 # spend too much time on this!
 
 #     |\_|X|_/|
@@ -19,32 +19,33 @@
 #   (   )---(   )
 #   Hello   Kitty
 
-# ------------------------------------ 
+# ------------------------------------
 # INSTRUCTIONS:
 
 f = open('rap.txt', 'r+')
 
+#this function should find all the unique words and put them in a dictionary
+#i.e dictionary = {'word' : 'count'}
 def unique_words():
-    words = {}
-    for line in f:
+    words = {} #dictionary that contains unique words
+    for line in f: #read each line in the file
         line = line.replace(',', '')
         line_stripped = [x.strip() for x in line.split(' ')]
-        for word in line_stripped:
-            if(word in words):
-                words[word] += 1
-            else:
-                words[word] = 1
-    return words
+        print line_stripped
+        #now you have an array (line_stripped) of the words in each line
+        #add them to your dictionary and keep track of count
+    pass
 
+#find the biggest vocab by finding the number of unique words
 def num_unique_words():
-    words = unique
-    return len(words)
-
-def top_words():
     words = unique_words()
-    import operator
-    sorted_words = sorted(words.items(), key=operator.itemgetter(1))
-    sorted_words_desc = sorted_words[::-1]
-    top_ten = sorted_words_desc[:10]
-    return top_ten
+    #return number of unique words
+    pass
 
+def top_ten_words():
+    words = unique_words()
+    import operator #sorts words by count
+    sorted_words = sorted(words.items(), key=operator.itemgetter(1))
+    print words
+    # now find the top 10 and return
+    pass
---
+++ puzzles/impossible/puzzle1iAnswers.py
@@ -0,0 +1,50 @@
+# ------------------------------------ 
+# CODE BOOLA 2015 PYTHON WORKSHOP
+# Mike Wu, Jonathan Chang, Kevin Tan
+# Puzzle Challenges Number 1
+# ------------------------------------ 
+
+# Crafted to really give you a hard
+# time. Please please please do not 
+# spend too much time on this!
+
+#     |\_|X|_/|
+#    /         \
+#  =(  O     O  )=
+#   -\    o    /-
+#    / .-----. \
+#  /_ | o   o |_ \
+# (U  |       |  U)
+#    _|_     _|_
+#   (   )---(   )
+#   Hello   Kitty
+
+# ------------------------------------ 
+# INSTRUCTIONS:
+
+f = open('rap.txt', 'r+')
+
+def unique_words():
+    words = {}
+    for line in f:
+        line = line.replace(',', '')
+        line_stripped = [x.strip() for x in line.split(' ')]
+        for word in line_stripped:
+            if(word in words):
+                words[word] += 1
+            else:
+                words[word] = 1
+    return words
+
+def num_unique_words():
+    words = unique
+    return len(words)
+
+def top_words():
+    words = unique_words()
+    import operator
+    sorted_words = sorted(words.items(), key=operator.itemgetter(1))
+    sorted_words_desc = sorted_words[::-1]
+    top_ten = sorted_words_desc[:10]
+    return top_ten
+
